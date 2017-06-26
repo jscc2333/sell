836a1fe0005a6617eadbd05d5e45f886c34a10cf
c <jscc2333@163.com>
2017-06-26 19:00:23 +0800

    complete component seller

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -54,7 +54,20 @@
     <split></split>
     <div class="pics">
       <h1 class="title">商家实景</h1>
-  
+      <div class="pic-wrapper" ref="picWrapper">
+        <ul class="pic-list" ref="picList">
+          <li class="pic-item" v-for="(pic,picIndex) in seller.pics" :key="picIndex">
+            <img :src="pic" width="120" height="90">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <split></split>
+    <div class="info">
+      <h1 class="title">商家信息</h1>
+      <ul>
+        <li class="info-item" v-for="(info,infoIndex) in seller.infos" :key="infoIndex">{{info}}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -89,14 +102,9 @@ export default {
       return this.favorite ? '已收藏' : '收藏';
     }
   },
-  watch: {
-    seller() {
-      this._initScroll();
-    }
-  },
-  mounted() {
+  created() {
     this.$nextTick(() => {
-      this._initScroll();
+      this._initPicsScroll();
     });
   },
   methods: {
@@ -104,14 +112,22 @@ export default {
       this.favorite = !this.favorite;
       saveToLocal(this.seller.id, 'favorite', this.favorite);
     },
-    _initScroll() {
-      if (!this.scroll) {
-        console.log('init');
-        this.scroll = new BScroll(this.$refs.seller, {
-          click: true
+    _initPicsScroll() {
+      if (this.seller.pics) {
+        let picWidth = 120;
+        let margin = 6;
+        let width = (picWidth + margin) * this.seller.pics.length - margin;
+        this.$refs.picList.style.width = width + 'px';
+        this.$nextTick(() => {
+          if (!this.picScroll) {
+            this.picScroll = new BScroll(this.$refs.picWrapper, {
+              scrollX: true,
+              eventPassthrough: 'vertical'
+            });
+          } else {
+            this.picScroll.refresh();
+          }
         });
-      } else {
-        this.scroll.refresh();
       }
     }
   }
@@ -222,7 +238,7 @@ export default {
         font-size: 0;
         .border-1px(rgba(7, 17, 27, 0.1));
         &:last-child {
-          border: none;
+         .border-none();
         }
         .text {
           display: inline-block;
@@ -233,6 +249,52 @@ export default {
           font-weight: 200;
           color: rgb(7, 17, 27);
         }
+      }
+    }
+  }
+  .pics {
+    padding: 18px;
+    .title {
+      margin-bottom: 12px;
+      line-height: 14px;
+      font-size: 14px;
+      color: rgb(7, 17, 27);
+    }
+    .pic-wrapper {
+      width: 100%;
+      overflow: hidden;
+      white-space: nowrap;
+      .pic-list {
+        font-size: 0;
+        .pic-item {
+          display: inline-block;
+          margin-right: 6px;
+          width: 120px;
+          height: 90px;
+          &:last-child {
+            margin-right: 0;
+          }
+        }
+      }
+    }
+  }
+  .info {
+    padding: 18px;
+    .title {
+      margin-bottom: 12px;
+      line-height: 14px;
+      font-size: 14px;
+      color: rgb(7, 17, 27);
+    }
+    .info-item {
+      padding: 16px 12px;
+      line-height: 16px;
+      font-size: 12px;
+      font-weight: 200;
+      color:rgb(7, 17, 27);
+      .border-1px(rgba(7,17,27,0.1));
+      &:last-child{
+        .border-none();
       }
     }
   }
